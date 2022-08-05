9d4c493bf3714eb2f8071e341fbf8d20051a7838
Shreyas Manolkar <shreyasmanolkar123@gmail.com>
2022-08-05 17:18:10 +0530

    export bug fixed

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,22 +5,23 @@ import { Flex, Box, Text, Button } from '@chakra-ui/react';
 import Property from '../components/Property';
 import { baseUrl, fetchApi } from '../utils/fetchApi';
 
-export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
+const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
-    <Image src={imageUrl} width={500} height={300} alt="image"/>
+    <Image src={imageUrl} width={500} height={300} alt="banner"/>
     <Box p='5'>
       <Text color='gray.500' fontSize='sm' fontWeight='medium'>{purpose}</Text>
       <Text fontSize='3xl' fontWeight='bold'>{title1}<br />{title2}</Text>
       <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>{desc1}<br />{desc2}</Text>
       <Button fontSize='xl' bg="blue.300" color="white">
-        <Link href={linkName}><a>{buttonText}</a></Link>
+        <Link href={linkName}>{buttonText}</Link>
       </Button>
     </Box>
   </Flex>
 );
 
 
-const Home = ({ propertiesForSale, propertiesForRent }) => (
+ export default function Home({ propertiesForSale, propertiesForRent }){
+  return(
   <Box>
     <Banner
       purpose='RENT A HOME'
@@ -49,7 +50,8 @@ const Home = ({ propertiesForSale, propertiesForRent }) => (
       {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
     </Flex>
   </Box>
-);
+)
+};
 
 export async function getStaticProps() {
   const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
@@ -61,6 +63,4 @@ export async function getStaticProps() {
       propertiesForRent: propertyForRent?.hits,
     },
   };
-}
-
-export default Home;
+};
